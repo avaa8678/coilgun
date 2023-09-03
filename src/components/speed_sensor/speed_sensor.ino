e15d7f85064e1e0baa5a305e52026702fb0c47a9
Annie Peng <133682389+ap0413@users.noreply.github.com>
2023-09-03 12:06:20 -0700

    Updated speed sensor (now has two speed sensors and display on lcd)
    Accuracy is pretty good
    LCD displays velocity

--- src/components/speed_sensor/speed_sensor.ino
+++ src/components/speed_sensor/speed_sensor.ino
@@ -1,55 +1,56 @@
-//one sensor, needs blocked -> not blocked, not blocked -> block to find speed with length of magnet
+#include <LiquidCrystal.h>// include the library code
 
-//TODO Need to clean up this code, for example, write wiring instruction etc.
-            
-int speedPin = 2;                        
+int speedPin1 = 2;      
+int speedPin2 = 4;                  
 
 int val = 0;
+int val2 = 0;
 int oldval = 0;
-long currtime;
-long newtime;
+long time2;
+long time1;
 
-double LENGTH = 200000.0;                
+double LENGTH = 12.5;     
+
+LiquidCrystal lcd(6, 7, 8, 9, 10, 11);
 
 
 void setup() {
-  pinMode(speedPin, INPUT); // declare sensor as input
+  pinMode(speedPin1, INPUT); // declare sensor as input
+  pinMode(speedPin2, INPUT);
+  lcd.begin(16, 2);
   Serial.begin(9600);  // for serial monitor
 }
 
 
 void loop() {
-  val = digitalRead(speedPin);
+  lcd.setCursor(0,0);
+  val = digitalRead(speedPin1);
   if (val == HIGH)
   {
+    val2 = digitalRead(speedPin2);
+    time1 = micros();
     Serial.print("Blocked, current val = ");
     Serial.println(val);
-    delay(5000); // to see speed
-    if (oldval == 0)
-    {
-      currtime = micros(); // get time in ms      
+    // delay(5000); // to see speed
+    while(val2 == 0) {
+      val2 = digitalRead(speedPin2);
+      if (val2 == HIGH) {
+        time2 = micros();
+        Serial.print("Time is ");
+        double timeLapse = (time2 - time1)/1000.0;
+        Serial.println(timeLapse);
+        Serial.println(time2-time1);
+        Serial.print("Velocity is ");
+        Serial.print(LENGTH/timeLapse); // v = d/t
+        lcd.print("Velocity: ");
+        lcd.setCursor(0,1);
+        lcd.print(LENGTH/timeLapse);
+        lcd.print(" mm/millis ");
+        Serial.println(" mm/millis ");
+
+
+      // delay(5000); // to see speed
+      }
     }
-    oldval = 1;
   }
-  else
-  {
-    Serial.println("NOT blocked");
-    if (oldval == 1)
-    {
-      newtime = micros(); // get time in ms
-      Serial.print("Time is ");
-
-      double timeLapse = (newtime - currtime)/1000.0;
-      Serial.println(timeLapse);
-      Serial.println(newtime-currtime);
-      Serial.print("Velocity is ");
-      Serial.print(LENGTH/timeLapse); // v = d/t
-      Serial.println(" mm/millis ");
-
-
-      delay(5000); // to see speed
-    }
-    oldval = 0; //reset val
-  }
- 
 }
