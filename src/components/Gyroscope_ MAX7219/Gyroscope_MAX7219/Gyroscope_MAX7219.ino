66d50681805c8a3fd56bdd310f8fc12b990dd697
avaa8678 <avaleichen@gmail.com>
2023-09-06 00:18:21 -0700

    updated complimentary filter and add comment about wiring

---
+++ src/components/Gyroscope_ MAX7219/Gyroscope_MAX7219/Gyroscope_MAX7219.ino
@@ -0,0 +1,276 @@
+/*
+Gyroscope + MAX7219
+
+Hardware Setup
+==================================================================================================
+Components: arduino nano, MPU 6050
+
+Nano 5v -> 6050 VCC
+Nano GND -> 6050 GND
+Nano A5 -> 6050 SCL
+Nano A4 -> 6050 SDA 
+
+Nano 5v -> MAX7219 VCC
+Nano GND -> MAX7219 GND
+Nano D10 -> MAX7219 CLK
+Nano D11 -> MAX7219 CS
+Nano D12 -> MAX7219 DIN
+*/
+
+#include <Wire.h>        // Include Wire Library for I2C
+#include <LedControl.h>  // Include for 8X8 Dot Matrix
+LedControl lc = LedControl(12, 10, 11, 1);
+
+/**********************************************************************/
+/******** BELOW ARE THE VALUE WE NEED TO TURN TO SET CENTER ***********/
+/**********************************************************************/
+// Complimentary Filter for roll and pitch
+const float rollCenterCali = 5.27;
+const float pitchCenterCali = -0.20;
+
+float step = 1;                   // this determine the LED moving steps
+const int calibrateTimes = 1000;  // How may iteration you want to calibrate the gyrometer
+const int tolerance = 20;         // Tolerance 0-10 = Ultra Fast - Loose accuracy but averages ok. For 11-100 is more acurrate but slower as you scale up. This is basically a delay on when you want to start
+const float rollCenter = 0;       // this determine the LED moving degree on roll when center
+const float pitchCenter = 0;      // this determine the LED moving degree on roll when center
+/**********************************************************************/
+/******************** END OF VALUE TUNNING ****************************/
+/**********************************************************************/
+
+const float pitchLow4 = pitchCenter - 4 * step;
+const float pitchLow3 = pitchCenter - 3 * step;
+const float pitchLow2 = pitchCenter - 2 * step;
+const float pitchLow1 = pitchCenter - step;  // center is between High1 > angleRollOutput > Low1, so it is in the middle of Low1 and High1
+const float pitchHigh1 = pitchCenter + step;
+const float pitchHigh2 = pitchCenter + 2 * step;
+const float pitchHigh3 = pitchCenter + 3 * step;
+const float pitchHigh4 = pitchCenter + 4 * step;
+const float rollLow4 = rollCenter - 4 * step;
+const float rollLow3 = rollCenter - 3 * step;
+const float rollLow2 = rollCenter - 2 * step;
+const float rollLow1 = rollCenter - step;  // center is between High1 > angleRollOutput > Low1, so it is in the middle of Low1 and High1
+const float rollHigh1 = rollCenter + step;
+const float rollHigh2 = rollCenter + 2 * step;
+const float rollHigh3 = rollCenter + 3 * step;
+const float rollHigh4 = rollCenter + 4 * step;
+
+int row, col, temp;                                          // row, column for LED, temperurate received but not used
+int displayCount = 0;                                        // Display counter
+int gyroX, gyroY, gyroZ, anglePitchBuffer, angleRollBuffer;  // Variables for Gyroscope
+long gyroXCal, gyroYCal, gyroZCal, accX, accY, accZ, accTotalVector;
+float angleRollAcc, anglePitchAcc, anglePitch, angleRoll, anglePitchOutput, angleRollOutput;
+boolean setGyroAngles;
+
+void setupMpu6050Registers() {
+  // Activate the MPU-6050
+  Wire.beginTransmission(0x68);  // Start communicating with the MPU-6050
+  Wire.write(0x6B);              // Send the requested starting register
+  Wire.write(0x00);              // Set the requested starting register
+  Wire.endTransmission();        // End the transmission
+
+  // Configure the accelerometer (+/-8g)
+  Wire.beginTransmission(0x68);  // Start communicating with the MPU-6050
+  Wire.write(0x1C);              // Send the requested starting register
+  Wire.write(0x10);              // Set the requested starting register
+  Wire.endTransmission();        // End the transmission
+
+  // Configure the gyro (500dps full scale)
+  Wire.beginTransmission(0x68);  // Start communicating with the MPU-6050
+  Wire.write(0x1B);              // Send the requested starting register
+  Wire.write(0x08);              // Set the requested starting register
+  Wire.endTransmission();        // End the transmission
+}
+
+void calibrateMpu6050() {
+  // Read the raw acc and gyro data from the MPU-6050 1000+ times
+  for (int calInt = 0; calInt < calibrateTimes; calInt++) {
+    readMpu6050Data();
+    gyroXCal += gyroX;  // Add the gyro x offset to the gyroXCal variable
+    gyroYCal += gyroY;  // Add the gyro y offset to the gyroYCal variable
+    gyroZCal += gyroZ;  // Add the gyro z offset to the gyroZCal variable
+    delay(3);           // Delay 3us to have 250Hz for-loop, I changed to 1us for loop of 2000
+  }
+
+  // Divide all results by calibrateTimes to get average offset
+  gyroXCal /= calibrateTimes;
+  gyroYCal /= calibrateTimes;
+  gyroZCal /= calibrateTimes;
+}
+
+void readMpu6050Data() {
+  // Read the raw gyro and accelerometer data
+  Wire.beginTransmission(0x68);  // Start communicating with the MPU-6050
+  Wire.write(0x3B);              // Send the requested starting register
+  Wire.endTransmission();        // End the transmission
+  Wire.requestFrom(0x68, 14);    // Request 14 bytes from the MPU-6050
+
+  // Wait until all the bytes are received
+  while (Wire.available() < 14)
+    ;
+
+  // Following statements left shift 8 bits, then bitwise OR. Turns two 8-bit values into one 16-bit value
+  accX = Wire.read() << 8 | Wire.read();
+  accY = Wire.read() << 8 | Wire.read();
+  accZ = Wire.read() << 8 | Wire.read();
+  temp = Wire.read() << 8 | Wire.read();
+  gyroX = Wire.read() << 8 | Wire.read();
+  gyroY = Wire.read() << 8 | Wire.read();
+  gyroZ = Wire.read() << 8 | Wire.read();
+}
+
+void setup() {
+  lc.shutdown(0, false);  // The MAX72XX is in power-saving mode on startup, we have to do a wakeup call
+
+  Wire.begin();             // Start I2C
+  setupMpu6050Registers();  // Setup the registers of the MPU-6050
+  calibrateMpu6050();       // Calibrate MPU-6050
+
+  Serial.begin(115200);  // Start Serial Monitor
+}
+
+void loop() {
+  readMpu6050Data();  // Get data from MPU-6050
+
+  // Subtract the offset values from the raw gyro values
+  gyroX -= gyroXCal;
+  gyroY -= gyroYCal;
+  gyroZ -= gyroZCal;
+
+  // Gyro angle calculations . Note 0.0000611 = 1 / (250Hz x 65.5)
+  anglePitch += gyroX * 0.0000611;  // Calculate the traveled pitch angle and add this to the anglePitch variable
+  angleRoll += gyroY * 0.0000611;   // Calculate the traveled roll angle and add this to the angleRoll variable
+
+  // 0.000001066 = 0.0000611 * (3.142(PI) / 180degr) The Arduino sin function is in radians
+  anglePitch += angleRoll * sin(gyroZ * 0.000001066);  // If the IMU has yawed transfer the roll angle to the pitch angle
+  angleRoll -= anglePitch * sin(gyroZ * 0.000001066);  // If the IMU has yawed transfer the pitch angle to the roll angle
+
+  // Accelerometer angle calculations
+  accTotalVector = sqrt((accX * accX) + (accY * accY) + (accZ * accZ));  // Calculate the total accelerometer vector
+
+  // 57.296 = 1 / (3.142 / 180) The Arduino asin function is in radians
+  anglePitchAcc = asin((float)accY / accTotalVector) * 57.296;  // Calculate the pitch angle
+  angleRollAcc = asin((float)accX / accTotalVector) * -57.296;  // Calculate the roll angle
+  anglePitchAcc -= pitchCenterCali;                             // Accelerometer calibration value for pitch
+  angleRollAcc -= rollCenterCali;                               // Accelerometer calibration value for roll
+
+  if (setGyroAngles) {
+    // If the IMU has been running
+    anglePitch = anglePitch * 0.9996 + anglePitchAcc * 0.0004;  // Correct the drift of the gyro pitch angle with the accelerometer pitch angle
+    angleRoll = angleRoll * 0.9996 + angleRollAcc * 0.0004;     // Correct the drift of the gyro roll angle with the accelerometer roll angle
+  } else {
+    // IMU has just started
+    anglePitch = anglePitchAcc;  // Set the gyro pitch angle equal to the accelerometer pitch angle
+    angleRoll = angleRollAcc;    // Set the gyro roll angle equal to the accelerometer roll angle
+    setGyroAngles = true;        // Set the IMU started flag
+  }
+
+  // To dampen the pitch and roll angles a complementary filter is used
+  anglePitchOutput = anglePitchOutput * 0.9 + anglePitch * 0.1;  // Take 90% of the output pitch value and add 10% of the raw pitch value
+  angleRollOutput = angleRollOutput * 0.9 + angleRoll * 0.1;     // Take 90% of the output roll value and add 10% of the raw roll value
+  // Wait until the loopTimer reaches 4000us (250Hz) before starting the next loop
+
+  displayCount = displayCount + 1;  // Increment the display counter
+
+  if (displayCount > tolerance) {
+    lc.clearDisplay(0);  // Clear the 8X8 display
+
+    // Get Col Position
+    if (angleRollOutput < rollLow4) {
+      row = 6;
+    } else if ((angleRollOutput > rollLow3) && (angleRollOutput < rollLow2)) {
+      row = 5;
+    } else if ((angleRollOutput > rollLow2) && (angleRollOutput < rollLow1)) {
+      row = 4;
+    } else if ((angleRollOutput > rollLow1) && (angleRollOutput < rollHigh1)) {
+      row = 3;
+    } else if ((angleRollOutput > rollHigh1) && (angleRollOutput < rollHigh2)) {
+      row = 2;
+    } else if ((angleRollOutput > rollHigh2) && (angleRollOutput < rollHigh3)) {
+      row = 1;
+    } else if (angleRollOutput > rollHigh4) {
+      row = 0;
+    }
+
+    // Get Row Position
+    if (anglePitchOutput < pitchLow4) {
+      col = 6;
+    } else if ((anglePitchOutput > pitchLow3) && (anglePitchOutput < pitchLow2)) {
+      col = 5;
+    } else if ((anglePitchOutput > pitchLow2) && (anglePitchOutput < pitchLow1)) {
+      col = 4;
+    } else if ((anglePitchOutput > pitchLow1) && (anglePitchOutput < pitchHigh1)) {
+      col = 3;
+    } else if ((anglePitchOutput > pitchHigh1) && (anglePitchOutput < pitchHigh2)) {
+      col = 2;
+    } else if ((anglePitchOutput > pitchHigh2) && (anglePitchOutput < pitchHigh3)) {
+      col = 1;
+    } else if (anglePitchOutput > pitchHigh4) {
+      col = 0;
+    }
+    /**************** DEBUGGING *********************/
+
+    Serial.print(" | Roll: ");
+    Serial.print(angleRollOutput);
+    Serial.print(" | Pitch: ");
+    Serial.print(anglePitchOutput);
+    Serial.print(" | Col: ");
+    Serial.print(col);
+    Serial.print(" | Row: ");
+    Serial.println(row);
+    /*    
+    Serial.print(" | RollLow4: ");
+    Serial.print(rollLow4);
+    Serial.print(" | RollLow3: ");
+    Serial.print(rollLow3);
+    Serial.print(" | RollLow2: ");
+    Serial.print(rollLow2);
+    Serial.print(" | RollLow1: ");
+    Serial.print(rollLow1);
+    Serial.print(" | RollHigh1: ");
+    Serial.print(rollHigh1);
+    Serial.print(" | RollHigh2: ");
+    Serial.print(rollHigh2);
+    Serial.print(" | RollHigh3: ");
+    Serial.print(rollHigh1);
+    Serial.print(" | RollHigh4: ");
+    Serial.print(rollHigh4);
+    Serial.print(" | PitchLow4: ");
+    Serial.print(pitchLow4);
+    Serial.print(" | PitchLow3: ");
+    Serial.print(pitchLow3);
+    Serial.print(" | PitchLow2: ");
+    Serial.print(pitchLow2);
+    Serial.print(" | PitchLow1: ");
+    Serial.print(pitchLow1);
+    Serial.print(" | PitchHigh1: ");
+    Serial.print(pitchHigh1);
+    Serial.print(" | PitchHigh2: ");
+    Serial.print(pitchHigh2);
+    Serial.print(" | PitchHigh3: ");
+    Serial.print(pitchHigh3);
+    Serial.print(" | PitchHigh4: ");
+    Serial.println(pitchHigh4);
+*/
+    lc.setIntensity(0, 8);  // Set the brightness to a medium value
+
+    // Light up the 4 LED quadrant of the position of row & col (x & y axis)
+    lc.setLed(0, col, row, true);
+    lc.setLed(0, col + 1, row, true);
+    lc.setLed(0, col, row + 1, true);
+    lc.setLed(0, col + 1, row + 1, true);
+
+    if ((row == 3) && (col == 3)) {
+      lc.setIntensity(0, 16);
+      lc.setRow(0, 0, B11111111);
+      for (int i = 1; i < 7; i++) {
+        lc.setRow(0, i, B10000001);
+      }
+      lc.setRow(0, 7, B11111111);
+    } else {
+      lc.setIntensity(0, 8);
+      lc.setRow(0, 3, B00011000);
+      lc.setRow(0, 4, B00011000);
+    }
+    displayCount = 0;
+  }
+}
